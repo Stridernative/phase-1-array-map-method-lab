e78f384dfdfdabec73749ec98730440549612e23
Gabe <GabeCreaCS@gmail.com>
2021-08-16 20:05:16 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,48 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// const correction = tutorials.map( fix => {
+   
+//   let word = tutorials.slice(str.length [0,1]) 
+//    return word
+
+    
+// }
+
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((phrase) => {
+   let splitSent = phrase.split(' ')
+   const word = splitSent.map((letter)  => letter.charAt(0).toUpperCase() + letter.slice(1) );
+   return word.join(" ");
+   })
+ }
+ titleCased();
+
+
+
+ 
+    //  let cap = word.charAt(0).toUpperCase(); 
+    //  let w = cap + word.slice(1);
+  //  let titleSent = [];
+  //  for (let i = 0; i < splitsent.length; i++){
+    //  let word = splitsent[i];
+
+
+
+
+
+// const titleCased = () => {
+//   return tutorials.map(string => {
+//     const eachWord = string.split (' ');
+//     const capitalizeEachWord =
+//       eachWord.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//     const result = capitalizeEachWord.join (' ');
+//     return result
+//   })
+// }
+// titleCased();
